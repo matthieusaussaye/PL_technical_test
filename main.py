66c411765a59dff7530218023f79e41065c4eae0
saussayematthieu <saussayematthieu50@gmail.com>
2021-09-28 17:18:27 +0200

    typing, cleaner notebook, docstrings & start the testfile

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import numpy as np
+from nptyping import *
+from typing import *
 
 def remove_duplicate(L : list,
-                     sort : bool = False) -> list :
+                     sort : bool = False) -> list : 
     """
     Remove all duplicated items from a list L 
     
@@ -12,33 +14,41 @@ def remove_duplicate(L : list,
     Returns: 
         - L_f (list) : List with no duplicated items.
     """
+    if not all([isinstance(L,list),isinstance(sort,bool)]) :
+        raise Exception(f'Incorrect types - L : {type(L)} instead of list, sort : {type(sort)} instead of bool')
 
     if sort : return list(set(L))
     else : return list(dict.fromkeys(L).keys()) 
 
-def permutations(string):
+def permutations(word : str) -> List[str] :
     """
     Create all permutations of a string with non-repeating characters
 
     Args: 
-        - string (str) : string to permute.
+        - word (str) : word to permute.
 
     Returns: 
-        - permutation_list (list) : List with every permutations of the string.
+        - permutation_list (list) : List with every permutations of the word.
     """
+
+    if not isinstance(word,str) :
+        raise Exception(f'Incorrect type : {type(word)}')
+
     permutation_list = []
-    if len(string) == 1:
-        return string
+
+
+    if len(word) == 1 or len(word) == 0 :
+        return [word]
 
     else:
-        for char in string:
-            for new_string in permutations(string.replace(char, "", 1)) :
+        for char in word:
+            for new_string in permutations(word.replace(char, "", 1)) :
                 #add at the extracted char every permutations lenght len(str)-1 without the extracted char
                 permutation_list.append(char + new_string)
 
     return permutation_list
 
-def signature(permutation : list) -> int :
+def signature(permutation : List[int]) -> int : #to add test 
     """
     Compute the signatures of a permutation (list of int)
 
@@ -48,6 +58,7 @@ def signature(permutation : list) -> int :
     Return:
         - prod (float) : signature of the permutation
     """
+
     prod = 1
     for j in range(len(permutation)) :
         for i in range(j) :
@@ -55,7 +66,7 @@ def signature(permutation : list) -> int :
 
     return(prod)
 
-def compute_determinant(M : np.ndarray) -> int :
+def compute_determinant(M : Union[NDArray[(int)],NDArray[(float)]]) -> int :
     """
     Compute the determinant of a square matrix using the signature.
 
@@ -66,8 +77,11 @@ def compute_determinant(M : np.ndarray) -> int :
         - determinant (float) : determinant of the matrix
     """
 
-    if not isinstance(M, np.ndarray) or len(M.shape) != 2 or M.shape[1] != M.shape[0] :
-        raise Exception('Matrix format incorrect. Input a np.ndarray matrix NxN with N>1')
+    if not isinstance(M, np.ndarray) :
+        raise Exception('Matrix type incorrect. Input a np.ndarray matrix NxN with N>1')
+    
+    if len(M.shape) != 2 or M.shape[1] != M.shape[0] :
+        raise Exception('Matrix size incorrect. Input a np.ndarray matrix NxN with N>1')
 
     if len(M) == 1 :
         return float(M)
@@ -91,9 +105,43 @@ def compute_determinant(M : np.ndarray) -> int :
     
     return(sum)
 
-def maxminexp(list_num : list,
-              list_op : list) -> tuple :
-	"""
+def operation(operator : str,
+              operand1 : Union[float,int],
+              operand2 : Union[float,int]) -> float :
+    
+    """
+    Return the operation with between the two operands & the operator.
+
+    Args:
+        - operator (str) : operator to use
+        - operand1 (float) : first operand
+        - operand2 (float) : second operand
+    
+    Return:
+        - result (float) : result of the operation
+    """
+
+    # if current operator is '+', updating tmp variable by addition
+    if(operator == '+'):
+        result = operand1 + operand2 #chose close operands to make next operation        
+
+    # if current operator is '*', updating tmp variable by multiplication
+    elif(operator == '*'):
+        result = operand1 * operand2 #chose close operands to make next operation        
+
+    # if current operator is '-', updating tmp variable by substraction
+    elif(operator == '-'):
+        result = operand1 - operand2 #chose close operands to make next operation        
+
+    # if current operator is '/', updating tmp variable by division
+    elif(operator == '/'):
+        result = operand1 / operand2 #chose close operands to make next operation        
+
+    return result
+
+def maxminexp(list_num : List[Union[int,float]],
+              list_op : List[str]) -> Tuple[str, tuple] :
+    """
     Compute the expression that gives the maximum & the difference between the max & the min expression for every possible parenthesis.
 
     Args:
@@ -103,76 +151,36 @@ def maxminexp(list_num : list,
     Return:
         - result (tuple) : maximal expression & rounded difference betweeen max & min.
     """
-	
-	llen = len(list_num)
-	minVal = [[ 0 for i in range(llen)] for i in range(llen)]
-	maxVal = [[ 0 for i in range(llen)] for i in range(llen)]
-	maxExpr = [[None for i in range(llen)] for i in range(llen)]
+    llen = len(list_num)
+    minVal = [[0 for i in range(llen)] for i in range(llen)]
+    maxVal = [[0 for i in range(llen)] for i in range(llen)]
+    maxExpr = [[None for i in range(llen)] for i in range(llen)]
+    # initializing minval and maxval 2D array
+    for i in range(llen) :
+        for j in range(llen) :
+            minVal[i][j] = 10**9
+            maxVal[i][j] = -10**9
+            if (i==j): # initializing main diagonal by num values
+                minVal[i][j] = maxVal[i][j] = list_num[i]
+                maxExpr[i][j] = str(list_num[i])
+    # looping similar to matrix chain multiplication
+    # and updating both 2D arrays
+    for L in range(2, llen + 1):
+        for i in range(llen - L + 1):
+            j = i + L - 1
+            for k in range(i,j):
+                minTmp = 0
+                maxTmp = 0
+                max_exprTmp = '('+maxExpr[i][k]+list_op[k]+maxExpr[k + 1][j]+')'
+                minTmp = operation(operator=list_op[k],operand1=minVal[i][k],operand2=minVal[k+1][j])
+                maxTmp = operation(operator=list_op[k],operand1=maxVal[i][k],operand2=maxVal[k+1][j])
+            
+                # updating array values by tmp variables
+                if (minTmp < minVal[i][j]) :
+                    minVal[i][j] = minTmp
+                if (maxTmp > maxVal[i][j]) :
+                    maxVal[i][j] = maxTmp
+                    maxExpr[i][j] = max_exprTmp
 
-
-	# initializing minval and maxval 2D array
-	for i in range(llen):
-		for j in range(llen):
-			minVal[i][j] = 10**9
-			maxVal[i][j] = -10**9
-
-			# initializing main diagonal by num values
-			if (i == j):
-				minVal[i][j] = maxVal[i][j] = list_num[i]
-				maxExpr[i][j] = str(list_num[i])
-
-	# looping similar to matrix chain multiplication
-	# and updating both 2D arrays
-	for L in range(2, llen + 1):
-		for i in range(llen - L + 1):
-			j = i + L - 1
-			for k in range(i, j):
-
-				minTmp = 0
-				maxTmp = 0
-				end_par = j-1
-
-				max_exprTmp = '('+maxExpr[i][k]+list_op[k]+maxExpr[k + 1][j]+')'
-
-				# if current operator is '+', updating tmp variable by addition
-				if(list_op[k] == '+'):
-
-					minTmp = minVal[i][k] + minVal[k + 1][j] #chose close operands to make next operation
-					maxTmp = maxVal[i][k] + maxVal[k + 1][j]
-					
-
-				# if current operator is '*', updating tmp variable by multiplication
-				elif(list_op[k] == '*'):
-
-					minTmp = minVal[i][k] * minVal[k + 1][j]
-					maxTmp = maxVal[i][k] * maxVal[k + 1][j]
-
-				# if current operator is '-', updating tmp variable by substraction
-				elif(list_op[k] == '-'):
-
-					minTmp = minVal[i][k] - minVal[k + 1][j]
-					maxTmp = maxVal[i][k] - maxVal[k + 1][j]
-
-				# if current operator is '/', updating tmp variable by division
-				elif(list_op[k] == '/'):
-
-					minTmp = minVal[i][k] / minVal[k + 1][j]
-					maxTmp = maxVal[i][k] / maxVal[k + 1][j]
-
-				# updating array values by tmp variables
-				if (minTmp < minVal[i][j]):
-					minVal[i][j] = minTmp
-					
-				if (maxTmp > maxVal[i][j]):
-					maxVal[i][j] = maxTmp
-					maxExpr[i][j] = max_exprTmp
-					
-
-	# last element of first row will store the result
-	print("Minimum value : ",minVal[0][llen - 1],", \
-			Maximum value : ",maxVal[0][llen - 1])
-	
-	diff = int(round(maxVal[0][llen - 1]-minVal[0][llen - 1]))
-
-	return(maxExpr[i][llen-1],diff)
-    
+    diff = int(round(maxVal[0][llen - 1]-minVal[0][llen - 1]))
+    return (maxExpr[i][llen-1],diff)
